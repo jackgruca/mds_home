2eae4266c9a64e8a354d0569565d14af1ab370a8
jackgruca <jackgruca@gmail.com>
2024-11-07 15:51:59 -0500

    Create team_needs_page.dart

---
+++ lib/screens/team_needs_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TeamNeedsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Team Needs will be displayed here.'),
+    );
+  }
+}
