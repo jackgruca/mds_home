8c7afe3b0b9c1f4d044c5f09b99f45e0adcb618d
jackgruca <jackgruca@gmail.com>
2024-11-07 15:51:53 -0500

    Update available_players_page.dart

--- lib/screens/available_players_page.dart
+++ lib/screens/available_players_page.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 
 class AvailablePlayersPage extends StatelessWidget {
-  final List<List<dynamic>> draftOrder;
+  final List<List<dynamic>> availablePlayers;
 
-  AvailablePlayersPage({required this.draftOrder});
+  AvailablePlayersPage({required this.availablePlayers});
 
   @override
   Widget build(BuildContext context) {
-    if (draftOrder.isEmpty || draftOrder.length <= 1) {
+    if (availablePlayers.isEmpty || availablePlayers.length <= 1) {
       return Center(child: CircularProgressIndicator());
     }
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemCount: draftOrder.length - 1,
-        itemBuilder: (context, index) {
-          final row = draftOrder[index + 1]; // Skip header row
-          return ListTile(
-            title: Text(row[1].toString()),
-            subtitle: Text('Pick: ${row[0]}, Previous Record: ${row[2]}'),
-          );
-        },
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: DataTable(
+          columns: [
+            DataColumn(label: Text('x')),
+            DataColumn(label: Text('Name')),
+            DataColumn(label: Text('Position')),
+          ],
+          rows: availablePlayers
+              .skip(1) // Skipping header row
+              .map(
+                (row) => DataRow(
+                  cells: [
+                    DataCell(Text(row[0].toString())),
+                    DataCell(Text(row[1].toString())),
+                    DataCell(Text(row[2].toString())),
+                  ],
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
-}
+}      
