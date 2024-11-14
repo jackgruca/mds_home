c79f0f2751b327f019095185bcd60dd243591a90
jackgruca <jackgruca@gmail.com>
2024-11-14 18:41:50 -0500

    Create draft_overview_screen.dart

---
+++ lib/screens/draft_overview_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'available_players_tab.dart';
+import 'team_needs_tab.dart';
+import 'draft_order_tab.dart';
+import '../../widgets/draft/draft_control_buttons.dart';
+
+class DraftApp extends StatefulWidget {
+  @override
+  _DraftAppState createState() => _DraftAppState();
+}
+
+class _DraftAppState extends State<DraftApp> {
+  int _selectedIndex = 0;
+  bool _isDraftRunning = false;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _toggleDraft() {
+    setState(() {
+      _isDraftRunning = !_isDraftRunning;
+    });
+  }
+
+  void _restartDraft() {
+    setState(() {
+      _isDraftRunning = false;
+      // Logic to reset the draft goes here
+    });
+  }
+
+  void _requestTrade() {
+    // Logic to request a trade goes here
+    print("Trade requested");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget currentPage;
+    switch (_selectedIndex) {
+      case 0:
+        currentPage = DraftOrderTab();
+        break;
+      case 1:
+        currentPage = AvailablePlayersTab();
+        break;
+      default:
+        currentPage = TeamNeedsTab();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NFL Draft'),
+      ),
+      body: currentPage,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Draft Order',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Available Players',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Team Needs',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: DraftControlButtons(
+        isDraftRunning: _isDraftRunning,
+        onToggleDraft: _toggleDraft,
+        onRestartDraft: _restartDraft,
+        onRequestTrade: _requestTrade,
+      ),
+    );
+  }
+}
