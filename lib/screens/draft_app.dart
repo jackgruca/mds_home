e2e044fbc02090e1651017e3b30e61d0a6d49791
jackgruca <jackgruca@gmail.com>
2024-11-14 14:51:30 -0500

    Update draft_app.dart

--- lib/screens/draft_app.dart
+++ lib/screens/draft_app.dart
@@ -5,15 +5,19 @@ import 'available_players_page.dart';
 import 'draft_order_page.dart';
 import 'team_needs_page.dart';
 
+bool _isLoadingAvailablePlayers = true;
+bool _isLoadingTeamNeeds = true;
+
 class DraftApp extends StatefulWidget {
   @override
   _DraftAppState createState() => _DraftAppState();
 }
 
 class _DraftAppState extends State<DraftApp> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
   List<List<dynamic>> _draftOrder = [];
   List<List<dynamic>> _availablePlayers = [];
+  List<List<dynamic>> _teamNeeds = [];
   int _numberOfRounds = 1;
 
   @override
@@ -21,6 +25,7 @@ class _DraftAppState extends State<DraftApp> {
     super.initState();
     _loadDraftOrder();
     _loadAvailablePlayers();
+    _loadTeamNeeds();
   }
 
   Future<void> _loadDraftOrder() async {
@@ -37,10 +42,33 @@ class _DraftAppState extends State<DraftApp> {
       List<List<dynamic>> csvTable = const CsvToListConverter().convert(data);
       setState(() {
         _availablePlayers = csvTable;
+        _isLoadingAvailablePlayers = false; // Set loading to false after data is loaded
+
       });
       print("Available Players Loaded: $_availablePlayers"); // Debugging statement
     } catch (e) {
       print("Error loading available players CSV: $e");
+      setState(() {
+        _isLoadingAvailablePlayers = false; // Stop loading state on error
+    });
+    }
+  }
+
+  Future<void> _loadTeamNeeds() async {
+    try {
+      final data = await rootBundle.loadString('assets/team_needs.csv');
+      List<List<dynamic>> csvTable = const CsvToListConverter().convert(data);
+      setState(() {
+        _teamNeeds = csvTable;
+        _isLoadingTeamNeeds = false; // Set loading to false after data is loaded
+
+      });
+      print("Team Needs Loaded: $_teamNeeds"); // Debugging statement
+    } catch (e) {
+      print("Error loading team needs CSV: $e");
+      setState(() {
+        _isLoadingTeamNeeds = false; // Stop loading state on error
+    });
     }
   }
 
@@ -50,45 +78,58 @@ class _DraftAppState extends State<DraftApp> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
+    Widget currentPage;
+    switch (_selectedIndex) {
+      case 0:
+        currentPage = Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Select Number of Rounds:'),
+                  DropdownButton<int>(
+                    value: _numberOfRounds,
+                    onChanged: (int? newValue) {
+                      setState(() {
+                        _numberOfRounds = newValue ?? 1;
+                      });
+                    },
+                    items: List.generate(7, (index) => index + 1)
+                        .map<DropdownMenuItem<int>>((int value) {
+                      return DropdownMenuItem<int>(
+                        value: value,
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: DraftOrderPage(draftOrder: _draftOrder)),
+          ],
+        );
+        break;
+      case 1:
+        currentPage = _isLoadingAvailablePlayers
+            ? Center(child: CircularProgressIndicator())
+            : AvailablePlayersPage(availablePlayers: _availablePlayers);
+        break;
+      default:
+        currentPage = _isLoadingTeamNeeds
+            ? Center(child: CircularProgressIndicator())
+            : TeamNeedsPage(teamNeeds: _teamNeeds);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('NFL Draft App'),
       ),
-      body: _selectedIndex == 0
-          ? Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Select Number of Rounds:'),
-                      DropdownButton<int>(
-                        value: _numberOfRounds,
-                        onChanged: (int? newValue) {
-                          setState(() {
-                            _numberOfRounds = newValue ?? 1;
-                          });
-                        },
-                        items: List.generate(7, (index) => index + 1)
-                            .map<DropdownMenuItem<int>>((int value) {
-                          return DropdownMenuItem<int>(
-                            value: value,
-                            child: Text(value.toString()),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(child: DraftOrderPage(draftOrder: _draftOrder)),
-              ],
-            )
-          : _selectedIndex == 1
-              ? AvailablePlayersPage(availablePlayers: _availablePlayers)
-              : TeamNeedsPage(),
+      body: currentPage,
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
