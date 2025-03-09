3c11815e06203e9b0454dc7646835367554f3f51
jackgruca <jackgruca@gmail.com>
2025-03-09 19:06:57 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,15 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'screens/team_selection_screen.dart';
-import 'utils/theme_config.dart';
-import 'utils/theme_manager.dart';
-
-import 'package:flutter/foundation.dart';
 
 void main() {
-  // Turn on debug output for the app
-  if (kDebugMode) {
-    debugPrint = (String? message, {int? wrapWidth}) {
-      if (message != null) {
-        print(message);
-      }
-    };
-  }
-  
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => ThemeManager(),
-      child: const MyApp(),
+    MaterialApp(
+      title: 'NFL Draft App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // Add additional theming here
+      ),
+      home: const TeamSelectionScreen(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<ThemeManager>(
-      builder: (context, themeManager, _) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'NFL Draft App',
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: themeManager.themeMode,
-          home: const TeamSelectionScreen(),
-        );
-      },
-    );
-  }
 }
